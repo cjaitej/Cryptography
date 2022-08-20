a1b7bb8273197b553941bf4909d8cfd5ba3c6db9
Jaitej <cjaitej@gmail.com>
2022-08-20 23:46:35 +0530

    Completed.

--- MonoalphabeticSubstitutionCipher.py
+++ MonoalphabeticSubstitutionCipher.py
@@ -5,6 +5,9 @@ class Monoalphabetic_Substitution_Cipher:
     Instead of shifting alphabets by fixed amount as CaeserCipher, alphabets are randomly shuffled and used as key.
     """
     def __init__(self):
+        """
+        Generating a random key.
+        """
         self.characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
         self.key = self.characters.copy()
         random.shuffle(self.key)
